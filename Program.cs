d6b23c1e011481d48786b5ee0875d0ad97fd55a2
Miroslav Popin <mpopin96@gmail.com>
2022-10-28 15:32:06 +0200

    uradjen zadatak 1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadata1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj: ");
+
+            string broj = Console.ReadLine();
+
+            bool nasao = false;
+
+            for (int i = 0; i < broj.Length; i++)
+            {
+                if (broj[i] == '3')
+                {
+                    Console.WriteLine("broj " + broj + " sadryi u sebi broj 3");
+                    break;
+                }
+
+            }
+
+            if (!nasao)
+            {
+                Console.WriteLine("broj " + broj + " NE sadryi u sebi broj 3");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
